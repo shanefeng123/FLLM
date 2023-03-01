Fixed a bug where all client parameters and server parameters are actually one list. Tried using gradient update to locate tokens in training batch but the loss is inf. Still figuring out why.

training/FL_avg_batch.py
@@ -25,14 +25,15 @@ if MODEL_NAME == "gpt2":
                                                   pad_token="<|pad|>")
 else:
     server = OpenAIGPTLMHeadModel.from_pretrained("openai-gpt").to(DEVICE)
-    tokenizer = OpenAIGPTTokenizerFast.from_pretrained("openai-gpt", bos_token="<|startoftext|>", eos_token="<|endoftext|>",
-                                                   pad_token="<|pad|>")
+    tokenizer = OpenAIGPTTokenizerFast.from_pretrained("openai-gpt", bos_token="<|startoftext|>",
+                                                       eos_token="<|endoftext|>",
+                                                       pad_token="<|pad|>")
 
 # Resize the token embedding of the model to match the new vocabulary size
 server.resize_token_embeddings(len(tokenizer))
 server_parameters = get_parameters(server)
 # Initialise client models
-client_parameters = [server_parameters] * NUM_OF_CLIENTS
+client_parameters = initialise_client_parameters(server_parameters, NUM_OF_CLIENTS)
 # Load the data
 data = load_enron_email_data(DATA_PATH)
 # Sample a subset to do the training to speed up the process
@@ -43,72 +44,74 @@ for i in range(len(data)):
     data[i] = "<|startoftext|>" + data[i] + "<|endoftext|>"
 # Split the data into training and testing
 train_data, test_data = train_test_split(data, test_size=TEST_SIZE, shuffle=True, random_state=42)
-num_of_client_samples = int(len(train_data) / NUM_OF_CLIENTS)
-clients_training_data = []
-# Split the training data into individual client training data
-for i in range(NUM_OF_CLIENTS):
-    clients_training_data.append(train_data[i * num_of_client_samples:(i + 1) * num_of_client_samples])
-
-train_loaders = []
-for i in range(NUM_OF_CLIENTS):
-    dataset = clients_training_data[i]
-    # Max length 128 would leave about 3 percent of the sentences truncated
-    data_inputs = tokenizer(dataset, return_tensors="pt", padding=True, truncation=True, max_length=128)
-    labels = data_inputs["input_ids"].clone()
-    data_inputs["labels"] = labels
-    data_loader = DataLoader(MyDataset(data_inputs), batch_size=128, shuffle=True)
-    train_loaders.append(data_loader)
-
-test_data_inputs = tokenizer(test_data, return_tensors="pt", padding=True, truncation=True, max_length=128)
-test_labels = test_data_inputs["input_ids"].clone()
-test_data_inputs["labels"] = test_labels
-test_loader = DataLoader(MyDataset(test_data_inputs), batch_size=128, shuffle=True)
-
-i = 0
-patience = 5
-patience_count = 0
-# This is for early stopping
-min_test_loss = 100000000
-min_loss_round = 0
-num_of_batches = len(train_loaders[0])
-while True:
-    client_losses = [0] * NUM_OF_CLIENTS
-    for j in range(num_of_batches):
-        print(f"Round {i}:")
-        client_loop = tqdm(range(NUM_OF_CLIENTS))
-        for k in client_loop:
-            client = set_parameters(server, client_parameters[k])
-            batch = train_loaders[k].__iter__().__next__()
-            batch_loss = train_batch(client, batch, device=DEVICE)
-            client_losses[k] += batch_loss
-            client_parameters[k] = get_parameters(client)
-        # do aggregation after every batch
-        aggregated_parameters = aggregate_parameters(server, client_parameters)
-        server = set_parameters(server, aggregated_parameters)
-        server_parameters = get_parameters(server)
-        client_parameters = [server_parameters] * NUM_OF_CLIENTS
-        print(f"Round {i}, server test loss:")
-        average_test_loss = test(server, test_loader, device=DEVICE)
-        with open(f"{RESULTS_PATH}", "a") as file:
-            file.write(f"Round {i}, server test loss: {average_test_loss}\n")
-        if average_test_loss > min_test_loss:
-            patience_count += 1
-        else:
-            min_test_loss = average_test_loss
-            min_loss_round = i
-            patience_count = 0
-        if patience_count >= patience:
-            torch.save(server.state_dict(), f"{MODEL_PATH}/server_{i}.pt")
-            print(f"Early stopping at round {i}")
-            print(f"Minimum test loss: {min_test_loss} at round {min_loss_round}")
-            with open(f"{RESULTS_PATH}", "a") as file:
-                file.write(f"Early stopping at round {i}\n")
-                file.write(f"Minimum test loss: {min_test_loss} at round {min_loss_round}\n")
-            break
-        i += 1
-
-    for j in range(NUM_OF_CLIENTS):
-        client_losses[j] = client_losses[j] / num_of_batches
-        print(f"Round {i}, client {j} average loss: {client_losses[j]}")
-        with open(f"{RESULTS_PATH}", "a") as file:
-            file.write(f"Round {i}, client {j} average loss: {client_losses[j]}\n")
+# num_of_client_samples = int(len(train_data) / NUM_OF_CLIENTS)
+# clients_training_data = []
+# # Split the training data into individual client training data
+# for i in range(NUM_OF_CLIENTS):
+#     clients_training_data.append(train_data[i * num_of_client_samples:(i + 1) * num_of_client_samples])
+#
+# train_loaders = []
+# for i in range(NUM_OF_CLIENTS):
+#     dataset = clients_training_data[i]
+#     # Max length 128 would leave about 3 percent of the sentences truncated
+#     data_inputs = tokenizer(dataset, return_tensors="pt", padding=True, truncation=True, max_length=128)
+#     labels = data_inputs["input_ids"].clone()
+#     data_inputs["labels"] = labels
+#     data_loader = DataLoader(MyDataset(data_inputs), batch_size=128, shuffle=True)
+#     train_loaders.append(data_loader)
+#
+# test_data_inputs = tokenizer(test_data, return_tensors="pt", padding=True, truncation=True, max_length=128)
+# test_labels = test_data_inputs["input_ids"].clone()
+# test_data_inputs["labels"] = test_labels
+# test_loader = DataLoader(MyDataset(test_data_inputs), batch_size=128, shuffle=True)
+#
+# i = 0
+# patience = 10
+# patience_count = 0
+# # This is for early stopping
+# min_test_loss = 100000000
+# min_loss_round = 0
+# num_of_batches = len(train_loaders[0])
+# early_stopping = False
+# while not early_stopping:
+#     client_losses = [0] * NUM_OF_CLIENTS
+#     for j in range(num_of_batches):
+#         print(f"Round {i}:")
+#         client_loop = tqdm(range(NUM_OF_CLIENTS))
+#         for k in client_loop:
+#             client = set_parameters(server, client_parameters[k])
+#             batch = train_loaders[k].__iter__().__next__()
+#             batch_loss = train_batch(client, batch, device=DEVICE)
+#             client_losses[k] += batch_loss
+#             client_parameters[k] = get_parameters(client)
+#         # do aggregation after every batch
+#         aggregated_parameters = aggregate_parameters(server, client_parameters)
+#         server = set_parameters(server, aggregated_parameters)
+#         server_parameters = get_parameters(server)
+#         client_parameters = initialise_client_parameters(server_parameters, NUM_OF_CLIENTS)
+#         print(f"Round {i}, server test loss:")
+#         average_test_loss = test(server, test_loader, device=DEVICE)
+#         with open(f"{RESULTS_PATH}", "a") as file:
+#             file.write(f"Round {i}, server test loss: {average_test_loss}\n")
+#         if average_test_loss > min_test_loss:
+#             patience_count += 1
+#         else:
+#             min_test_loss = average_test_loss
+#             min_loss_round = i
+#             patience_count = 0
+#         if patience_count >= patience:
+#             torch.save(server.state_dict(), f"{MODEL_PATH}/server_{i}.pt")
+#             print(f"Early stopping at round {i}")
+#             print(f"Minimum test loss: {min_test_loss} at round {min_loss_round}")
+#             with open(f"{RESULTS_PATH}", "a") as file:
+#                 file.write(f"Early stopping at round {i}\n")
+#                 file.write(f"Minimum test loss: {min_test_loss} at round {min_loss_round}\n")
+#             early_stopping = True
+#             break
+#         i += 1
+#
+#         if j % 10 == 0:
+#             for l in range(NUM_OF_CLIENTS):
+#                 print(f"Round {i - 1}, client {l} average loss: {client_losses[l] / (j + 1)}")
+#                 with open(f"{RESULTS_PATH}", "a") as file:
+#                     file.write(f"Round {i - 1}, client {l} average loss: {client_losses[l] / (j + 1)}\n")

training/FL_avg_epoch.py
@@ -32,7 +32,7 @@ else:
 server.resize_token_embeddings(len(tokenizer))
 server_parameters = get_parameters(server)
 # Initialise client models
-client_parameters = [server_parameters] * NUM_OF_CLIENTS
+client_parameters = initialise_client_parameters(server_parameters, NUM_OF_CLIENTS)
 # Load the data
 data = load_enron_email_data(DATA_PATH)
 # Sample a subset to do the training to speed up the process
@@ -91,7 +91,7 @@ while True:
     average_test_loss = test(server, test_loader, device=DEVICE)
     with open(f"{RESULTS_PATH}", "a") as file:
         file.write(f"Round {i} server test loss: {average_test_loss}\n")
-    client_parameters = [server_parameters] * NUM_OF_CLIENTS
+    client_parameters = initialise_client_parameters(server_parameters, NUM_OF_CLIENTS)
     torch.save(server.state_dict(), f"{MODEL_PATH}/server_{i}.pt")
     if average_test_loss > min_test_loss:
         patience_count += 1

utils/__init__.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import pandas as pd
 import nltk
+import numpy as np
 
 
 class MyDataset(torch.utils.data.Dataset):
@@ -60,12 +61,28 @@ def get_parameters(model):
     """
     Get the parameters of a model.
     Args:
-        model: A neural network models with parameters.
+        model: A neural network model with parameters.
 
     Returns:
-        A list of parameters of the model.
+        A deep copy list of parameters of the model.
     """
-    return [val.cpu().numpy() for _, val in model.state_dict().items()]
+    return [val.clone().detach().cpu().numpy() for _, val in model.state_dict().items()]
+
+
+def initialise_client_parameters(server_parameters, num_of_clients):
+    """
+    Initialise the parameters of the clients.
+    Args:
+        server_parameters: The parameters of the server model.
+        num_of_clients: The number of clients.
+
+    Returns:
+        A list of parameters for the clients.
+    """
+    client_parameters = []
+    for i in range(num_of_clients):
+        client_parameters.append([np.copy(p) for p in server_parameters])
+    return client_parameters
 
 
 def set_parameters(model, parameters):
@@ -179,3 +196,16 @@ def aggregate_parameters(server, client_parameters):
     for j in range(len(aggregated_parameters)):
         aggregated_parameters[j] /= len(client_parameters)
     return aggregated_parameters
+
+
+def cosine_similarity(vector_a, vector_b):
+    """
+    Calculate the cosine similarity between two vectors.
+    Args:
+        vector_a: A vector.
+        vector_b: Another vector.
+
+    Returns:
+        The cosine similarity between the two vectors.
+    """
+    return np.dot(vector_a, vector_b) / (np.linalg.norm(vector_a) * np.linalg.norm(vector_b))